8bf6676f764f11518e79e8140430533222478b02
Franck <fran.det17@gmail.com>
2019-02-26 23:28:18 +0100

    Separated apps urls

--- intra_junior/urls.py
+++ intra_junior/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from intra import views
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.rejoindre, name='rejoindre'),
-    path('faq',views.faq, name='faq'),
-    path('moncompte',views.moncompte, name='moncompte'),
+    path('intra/',include('intra.urls')),
 ]
